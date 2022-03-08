ba90202ae36ebacf6fb0fc7cbb1defdb83d619c5
Kasper Dahl Andersen <kasperdahldk@gmail.com>
2022-03-08 15:27:31 +0100

    obj. func. L_M somewhat optimized

--- EM/ExpectationMaximization.py
+++ EM/ExpectationMaximization.py
@@ -6,9 +6,9 @@ from bisect import bisect_left
 from time import perf_counter as pc
 
 
-dataset = pd.read_csv(
-    "C:/thesis_code/Github/data/comp_sets/thy_parishes_1850_1845")
-# dataset = pd.read_csv("C:/thesis_code/Github/data/comp_sets/junget_1850_1845")
+# dataset = pd.read_csv(
+#     "C:/thesis_code/Github/data/comp_sets/thy_parishes_1850_1845")
+dataset = pd.read_csv("C:/thesis_code/Github/data/comp_sets/junget_1850_1845")
 # dataset = pd.read_csv("C:/thesis_code/Github/data/comp_sets/testset")
 # dataset = pd.read_csv("C:/thesis_code/Github/data/comp_sets/testset_2d")
 
@@ -56,8 +56,12 @@ class ExpectationMaximization:
             self.p_M = np.mean(w_vector)
             self.p_U = 1 - self.p_M
 
-            x_M = [self.P_A_M, self.P_FN_M, self.P_LN_M]
-            x_U = [self.P_A_U, self.P_FN_U, self.P_LN_U]
+            # x_M = [self.P_A_M, self.P_FN_M, self.P_LN_M]
+            # x_U = [self.P_A_U, self.P_FN_U, self.P_LN_U]
+            x_M = np.array([self.P_A_M, self.P_FN_M, self.P_LN_M])
+            x_U = np.array([self.P_A_U, self.P_FN_U, self.P_LN_U])
+
+            #log_p = np.array([log_p_a, log_p_f, log_p_l])
 
             res_M = minimize(self.L_M, x0=x_M, args=(self.data, w_vector),
                              method='L-BFGS-B', bounds=[(0.001, 1), (0.001, 1), (0.001, 1)])
@@ -77,15 +81,15 @@ class ExpectationMaximization:
         print(f"p_M: {self.p_M} \n p_U: {self.p_U}")
         return [self.P_A_M, self.P_FN_M, self.P_LN_M, self.P_A_U, self.P_FN_U, self.P_LN_U]
 
-    def L_M(self, x, data, w_vector):
-        log_p_a = np.log(x[0]+x[0]**2+x[0]**3)
-        log_p_f = np.log(x[1]+x[1]**2+x[1]**3+x[1]**4)
-        log_p_l = np.log(x[2]+x[2]**2+x[2]**3+x[2]**4)
-        sum = 0
-        for i in range(len(data)):
-            sum += w_vector[i]*(data[i][0]*np.log(x[0])-log_p_a+data[i][1]
-                                * np.log(x[1])-log_p_f+data[i][2]*np.log(x[2])-log_p_l)
-        return -1*sum
+    # def L_M(self, x, data, w_vector):
+    #     log_p_a = np.log(x[0]+x[0]**2+x[0]**3)
+    #     log_p_f = np.log(x[1]+x[1]**2+x[1]**3+x[1]**4)
+    #     log_p_l = np.log(x[2]+x[2]**2+x[2]**3+x[2]**4)
+    #     sum = 0
+    #     for i in range(len(data)):
+    #         sum += w_vector[i]*(data[i][0]*np.log(x[0])-log_p_a+data[i][1]
+    #                             * np.log(x[1])-log_p_f+data[i][2]*np.log(x[2])-log_p_l)
+    #     return -1*sum
 
     def L_U(self, x, data, w_vector):
         log_p_a = np.log(x[0]+x[0]**2+x[0]**3)
@@ -96,6 +100,30 @@ class ExpectationMaximization:
             sum += (1-w_vector[i])*((3-data[i][0])*np.log(x[0])-log_p_a+(4-data[i][1])
                                     * np.log(x[1])-log_p_f+(4-data[i][2])*np.log(x[2])-log_p_l)
         return -1*sum
+
+    def L_M(self, x, data, w_vector):
+        # brug disse som argumenter i minimize i stedet - muligvis også disse: np_log_a = np.log(x[0])
+        log_p_a = np.log(x[0]+x[0]**2+x[0]**3)
+        log_p_f = np.log(x[1]+x[1]**2+x[1]**3+x[1]**4)
+        log_p_l = np.log(x[2]+x[2]**2+x[2]**3+x[2]**4)
+        result_1 = data*np.log(x)
+        result_1[:, 0] = result_1[:, 0]-log_p_a
+        result_1[:, 1] = result_1[:, 1]-log_p_f
+        result_1[:, 2] = result_1[:, 2]-log_p_l
+        result = np.sum(w_vector * np.sum(result_1, axis=1))
+        return -result
+
+    # def L_U(self, x, data, w_vector):
+    #     # brug disse som argumenter i minimize i stedet - muligvis også disse: np_log_a = np.log(x[0])
+    #     log_p_a = np.log(x[0]+x[0]**2+x[0]**3)
+    #     log_p_f = np.log(x[1]+x[1]**2+x[1]**3+x[1]**4)
+    #     log_p_l = np.log(x[2]+x[2]**2+x[2]**3+x[2]**4)   #
+    #     result_1 = data*np.log(x)
+    #     result_1[:, 0] = result_1[:, 0]-log_p_a
+    #     result_1[:, 1] = result_1[:, 1]-log_p_f
+    #     result_1[:, 2] = result_1[:, 2]-log_p_l    #
+    #     result = np.sum((1-w_vector) * np.sum(result_1, axis=1))
+    #     return -result
 
     # def L_M(self, x, data, w_vector):
     #     length = len(data)
@@ -181,9 +209,9 @@ dataset_values = np.array([dist_age, dist_fn, dist_ln]).transpose()
 
 # TEST CLASS
 print(f"starting CLASS")
-em = ExpectationMaximization(dataset_values, 2200)
+em = ExpectationMaximization(dataset_values, 100)
 results = em.em_steps(7)
-#results = [0.001, 0.50860069, 0.001, 0.71309623, 0.21479873, 0.30512826]
+# results = [0.001, 0.50860069, 0.001, 0.71309623, 0.21479873, 0.30512826]
 print(f"Results: \n {results}")
-em.evaluation_bayes(dataset_values, results, data_bisect)
-#print(f"Probability results: \n {bayes}")
+#em.evaluation_bayes(dataset_values, results, data_bisect)
+# print(f"Probability results: \n {bayes}")
